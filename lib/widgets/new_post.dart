7d606ceba3330f53c9be59653d6d93c1738c3c3d
Teerasej <teerasej@gmail.com>
2018-10-02 13:46:43 +0700

    Add card layout

--- lib/widgets/new_post.dart
+++ lib/widgets/new_post.dart
@@ -8,59 +8,59 @@ class _NewPostState extends State<NewPost> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 100.0,
+        // height: 100.0,
+        padding: EdgeInsets.all(10.0),
         color: Colors.redAccent,
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  height: 40.0,
-                  width: 40.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage("https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/15267619_10154789415276098_1443357584742520709_n.jpg?_nc_cat=103&_nc_eui2=AeHuPeABmNHVHNpJgJ00gVIAjRAxxXJZ_96UDcsaOCG8-e3G_brNJ_nMrKTDV1Wx-qMAz1pw_XBqcY3YhY87vgU1-py_G_VSaJy6RHQUHjrBAQ&oh=c6ccfa1910c5e9908fc779903f1b78db&oe=5C5F4BA1")
-                    )
+        child: Card(
+          child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/15267619_10154789415276098_1443357584742520709_n.jpg?_nc_cat=103&_nc_eui2=AeHuPeABmNHVHNpJgJ00gVIAjRAxxXJZ_96UDcsaOCG8-e3G_brNJ_nMrKTDV1Wx-qMAz1pw_XBqcY3YhY87vgU1-py_G_VSaJy6RHQUHjrBAQ&oh=c6ccfa1910c5e9908fc779903f1b78db&oe=5C5F4BA1")))),
+                      Text("What's your mind?")
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.videocam),
+                            Text('Live')
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[Icon(Icons.photo), Text('Photos')],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.place),
+                            Text('Check-in')
+                          ],
+                        ),
+                      )
+                    ],
                   )
-                ),
-                Text("What's your mind?")
-              ],
-            ),
-            Row( 
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.videocam),
-                      Text('Live')
-                    ],
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.photo),
-                      Text('Photos')
-                    ],
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.place),
-                      Text('Check-in')
-                    ],
-                  ),
-                )
-              ],
-            )
-          ],
+                ],
+              )),
         ));
   }
 }
