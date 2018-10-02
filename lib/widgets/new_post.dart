622ad0daf67bb868c49ec26904550ca3020ba065
Teerasej <teerasej@gmail.com>
2018-10-02 13:25:41 +0700

    Make photo circle

--- lib/widgets/new_post.dart
+++ lib/widgets/new_post.dart
@@ -14,10 +14,16 @@ class _NewPostState extends State<NewPost> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Image.network(
-                  'https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/15267619_10154789415276098_1443357584742520709_n.jpg?_nc_cat=103&_nc_eui2=AeHuPeABmNHVHNpJgJ00gVIAjRAxxXJZ_96UDcsaOCG8-e3G_brNJ_nMrKTDV1Wx-qMAz1pw_XBqcY3YhY87vgU1-py_G_VSaJy6RHQUHjrBAQ&oh=c6ccfa1910c5e9908fc779903f1b78db&oe=5C5F4BA1',
+                Container(
                   height: 40.0,
                   width: 40.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage("https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/15267619_10154789415276098_1443357584742520709_n.jpg?_nc_cat=103&_nc_eui2=AeHuPeABmNHVHNpJgJ00gVIAjRAxxXJZ_96UDcsaOCG8-e3G_brNJ_nMrKTDV1Wx-qMAz1pw_XBqcY3YhY87vgU1-py_G_VSaJy6RHQUHjrBAQ&oh=c6ccfa1910c5e9908fc779903f1b78db&oe=5C5F4BA1")
+                    )
+                  )
                 ),
                 Text("What's your mind?")
               ],
